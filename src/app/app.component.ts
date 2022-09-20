b1de6ee14d4a04080b4d99b72ef133c903a93f43
germancai <germancai@hotmail.com>
2022-09-20 12:52:38 -0300

    aplicacion presupuesto terminada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Egreso } from './egreso/egreso.model';
+import { EgresoServicio } from './egreso/egreso.servicio';
+import { Ingreso } from './ingreso/ingreso.model';
+import { IngresoServicio } from './ingreso/ingreso.servicio';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'presupuesto-app';
+  ingresos:Ingreso[]= [];
+  egresos:Egreso[]= [];
+
+
+  constructor(private ingresoServicio:IngresoServicio, 
+              private egresoServicio:EgresoServicio) {
+                this.ingresos = ingresoServicio.ingreso;
+                this.egresos = egresoServicio.egresos;
+              }
+
+
+  getIngresoTotal(){
+    let ingresoTotal = 0;
+    this.ingresos.forEach(ingreso => {
+      ingresoTotal += ingreso.valor;
+    })
+    return ingresoTotal;
+  }            
+
+  getEgresoTotal(){
+    let egresoTotal = 0;
+    this.egresos.forEach(egreso => {
+      egresoTotal += egreso.valor;
+    })
+    return egresoTotal;
+  }
+
+  getPorcentajeTotal(){
+    return this.getEgresoTotal()/this.getIngresoTotal();
+  }
+
+  getPresupuestoTotal(){
+    return this.getIngresoTotal()-this.getEgresoTotal();
+  }
+
 }
---
+++ src/app/ingreso/ingreso.servicio.ts
@@ -0,0 +1,13 @@
+import { Ingreso } from "./ingreso.model";
+
+export class IngresoServicio {
+    ingreso:Ingreso[]=[
+        new Ingreso("Salario", 4000),
+        new Ingreso("Venta del auto", 500)
+    ]
+
+    eliminar(ingreso: Ingreso){
+        const indice: number = this.ingreso.indexOf(ingreso);
+        this.ingreso.splice(indice, 1);
+    }
+}
